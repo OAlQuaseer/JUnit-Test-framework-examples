6e8fe08903a52ea4f54bbd98f3d06498cd80ffaa
OAlQuaseer <omarqf87@gmail.com>
2016-04-16 18:01:49 -0700

    Creating a maven project

---
+++ JUnit-Test-framework-examples/src/main/java/JUnit_Test_framework_examples/JUnit_Test_framework_examples/App.java
@@ -0,0 +1,13 @@
+package JUnit_Test_framework_examples.JUnit_Test_framework_examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
