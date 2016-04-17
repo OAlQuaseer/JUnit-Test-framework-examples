9d59aa7e0ad949c741fbb0453894e5cc39c7ab02
OAlQuaseer <omarqf87@gmail.com>
2016-04-17 16:55:15 -0700

    example 1 Fixture feature.
    Fixture feature :-
    
    1- setUp() method which runs before every test invocation.
    2- tearDown() method which runs after every test method.

---
+++ JUnit-Test-framework-examples/src/test/java/com/oalquaseer/junit/Example1.java
@@ -0,0 +1,25 @@
+package com.oalquaseer.junit;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for Fixture feature in JUnit test framework.
+ */
+public class Example1 extends TestCase {
+	
+	protected Integer value1, value2;
+	
+	// this method runs before every method invocation.
+	protected void setUp(){
+		value1 = 3;
+		value2 = 3;
+	}
+	
+	// test method to test adding two values
+	public void testAdd (){
+		
+		assertTrue(value1+value2 == 6);
+		
+		
+	}
+}
